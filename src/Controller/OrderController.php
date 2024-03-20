03d0a2aceb8789d804933ecf33f8baaa3768b6bf
nasim-saaidi <302263973@student.rocmondriaan.nl>
2024-03-20 11:35:17 +0100

    resume order form

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\Product;
 use App\Form\OrderType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,12 +14,58 @@ use Symfony\Component\Routing\Attribute\Route;
 class OrderController extends AbstractController
 {
     #[Route('/order/{product}', name: 'app_order')]
-    public function new(EntityManagerInterface $entityManager, Request $request): Response
+    public function new(EntityManagerInterface $entityManager, Request $request, $product): Response
     {
         $form = $this->createForm(OrderType::class);
+
+        //maken form uit de type
+        $form = $this->createForm(OrderType::class);
+
+        //symfony verwerkt formulier
+        $form->handleRequest($request);
+
+        //meesturen/ophalen van het product
+        $product = $entityManager->getRepository(Product::class)->find($product);
+
         if($form->isSubmitted() && $form->isValid()) {
-            echo "bruh";
+            //data halen uit formulir, geeft gevulde entity aan
+            $task = $form->getData();
+
+            //voeren de entity in de entitymanager in
+            $entityManager->persist($task);
+
+            //spoelen de entityManager, zo komt de data in de database
+            $entityManager->flush();
+
+            //flash message aanmaken
+            $this->addFlash(
+                'success',
+                'De order is toegevoegd'
+            );
+
+            $order = $form->getData();
+            $order->setProduct($product);
+            $size = $order->getSize();
+            $order->setProduct();
+
+            if ($size === 'v-8') {
+                $extraPrice = 0;
+            }
+            if ($size === 'v-12') {
+                $extraPrice = 100;
+            }
+            if ($size === 'v-16') {
+                $extraPrice = 200;
+            }
+
+            //stuur naar ander page
+            return $this->redirectToRoute('app_front');
+
+
+
         }
+
+
         $order = $entityManager->getRepository(Order::class)->findAll();
         return $this->render('order/new.html.twig', [
             'form' =>$form,
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -6,6 +6,8 @@ use App\Entity\Order;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,13 +17,20 @@ class OrderType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('first_name')
-            ->add('last_name')
-            ->add('street_name')
-            ->add('house_number')
-            ->add('zip_code')
-            ->add('city')
-            ->add('size')
+            ->add('first_name', TextType::class, ['label' => 'voornaam'])
+            ->add('last_name', TextType::class, ['label' => 'achternaam'])
+            ->add('street_name', TextType::class, ['label' => 'straatnaam'])
+            ->add('house_number', TextType::class, ['label' => 'huisnummer'])
+            ->add('zip_code', TextType::class, ['label' => 'zipcode'])
+            ->add('city', TextType::class, ['label' => 'stad'])
+            ->add('size', ChoiceType::class, [
+                'label' => 'motor',
+                'choices' => [
+                    'v-8 motor + 0 euro' => 'v-8',
+                    'v-12 motor + 100 euro ' => 'v-12',
+                    'v-16 motor + 200 euro' => 'v-16'
+                ]
+            ])
             ->add('product', EntityType::class, [
                 'class' => Product::class,
                 'choice_label' => 'id',
